1507c7f68c1e6197e2fc8ca0e2f22ba321606f73
Bishnu <thapabishnu20@gmail.com>
2023-09-25 14:58:33 +0545

    news api integrated & retrive news data successfully

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, HttpResponse
-
+import requests
+API_KEY = '9c82badaf452409db67d5d83ee66a05e'
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("ok")
+    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    news = data['articles']
+    context = {
+        'news': news
+    }
+    return render(request, 'home.html', context)
