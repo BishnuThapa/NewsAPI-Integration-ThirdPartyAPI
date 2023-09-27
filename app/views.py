ffce2f4ee11127d1f49bdcdaf46b432c3f024e9f
Bishnu <thapabishnu20@gmail.com>
2023-09-27 11:24:57 +0545

    added search by country functionality

--- app/views.py
+++ app/views.py
@@ -5,7 +5,8 @@ API_KEY = '9c82badaf452409db67d5d83ee66a05e'
 
 
 def home(request):
-    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
+    country=request.GET.get('country')
+    url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
     response = requests.get(url)
     data = response.json()
     news = data['articles']
